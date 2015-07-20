6fe74c51a531231d4bf5e16953b62985996de823
acerix <dylan@demcra.com>
2015-07-20 23:51:24 +0000

    new error imgs

--- views/site/error.php
+++ views/site/error.php
@@ -13,9 +13,38 @@ $this->title = $name;
 
     <h1><?=Html::encode($this->title)?></h1>
 
+<?php
+switch ($exception->statusCode)
+{
+
+    case 404:
+?>
     <div style="text-align:center;margin:20px auto;">
-        <img src="/img/404.png" alt="">
+        <img src="/img/404.png" alt="" style="width:100%;">
     </div>
+<?php
+        break;
+
+    case 451:
+?>
+    <div style="text-align:center;margin:20px auto;">
+        <img src="/img/451.png" alt="" style="width:100%;">
+    </div>
+<?php
+        break;
+
+    default:
+?>
+    <div style="text-align:center;margin:20px auto;">
+        <img src="/img/500.png" alt="">
+    </div>
+<?php
+        break;
+
+}
+
+?>
+
 
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
